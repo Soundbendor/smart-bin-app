66fb6c232cb8b51b3aa9660dc64c721939f122c3
stalberm <77121289+stalberm@users.noreply.github.com>
2023-10-28 10:01:40 -0700

    Page navigation (#7)
    * update with simple description and project team names
    
    * Adds basic layout of app with router functionality
    
    ---------
    
    Co-authored-by: Kimberly Markley <markleki@oregonstate.edu>
    Co-authored-by: Kimberly Markley <73560521+kimberlymarkley@users.noreply.github.com>

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,65 @@
 import 'package:flutter/material.dart';
+import 'package:waste_watchers/screens/detections_page.dart';
+import 'package:waste_watchers/screens/home_page.dart';
+import 'package:waste_watchers/screens/stats_page.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(const MainPage());
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class MainPage extends StatefulWidget {
+  const MainPage({Key? key}) : super(key: key);
+
+  @override
+  State<MainPage> createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  int index = 0;
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+        appBar: AppBar(
+          title: const Text("Waste Watchers"),
+          centerTitle: true,
+        ),
+        body: IndexedStack(
+          index: index,
+          children: const [
+            HomePage(),
+            DetectionsPage(),
+            StatsPage(),
+          ],
+        ),
+        bottomNavigationBar: NavigationBarTheme(
+          data: const NavigationBarThemeData(
+            labelTextStyle: MaterialStatePropertyAll(
+              TextStyle(fontSize: 14, fontWeight: FontWeight.w500),
+            ),
+          ),
+          child: NavigationBar(
+            height: 60,
+            selectedIndex: index,
+            onDestinationSelected: (index) => setState(() {
+              this.index = index;
+            }),
+            destinations: const [
+              NavigationDestination(
+                icon: Icon(Icons.home),
+                label: 'Home',
+              ),
+              NavigationDestination(
+                icon: Icon(Icons.batch_prediction),
+                label: 'Detections',
+              ),
+              NavigationDestination(
+                icon: Icon(Icons.analytics),
+                label: 'Stats',
+              ),
+            ],
+          ),
         ),
       ),
     );
