ab8f6ba35ea8dfe14f3402bb374645e7e76a2c6e
unknown <pughbl@oregonstate.edu>
2023-11-07 14:53:20 -0800

    Splash screen added to /lib/screens, looks pretty :), main updated to accommodate it being the first screen when you open the app.

---
+++ lib/screens/splash_page.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+
+class SplashPage extends StatelessWidget {
+  const SplashPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage("assets/images/background.JPG"),
+            fit: BoxFit.cover
+            ),
+            ),
+        child: Center(
+          child: Column(
+            children: [
+              Flexible(
+                flex: 5,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                  const Padding(
+                    padding: EdgeInsets.all(20),
+                    child: Text(
+                      "Welcome!",
+                      style: TextStyle(
+                        fontSize: 40,
+                        fontWeight: FontWeight.bold
+                        ),
+                        )
+                    ),
+                  const Text(
+                    "Connect your Smart Bin to WiFi to start collecting data!",
+                    style: TextStyle(fontSize: 25, ),
+                    textAlign: TextAlign.center,
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: TextButton(
+                        style: TextButton.styleFrom(
+                          foregroundColor: Colors.white,
+                          padding: const EdgeInsets.all(16.0),
+                          textStyle: const TextStyle(fontSize: 20),
+                          backgroundColor: const Color(0xFF15a2cd)
+                        ),
+                        onPressed: () {},
+                        child: const Text('Continue'),
+                      ),
+                    )
+                  ],
+                ),
+              ),
+              Flexible(flex: 3, child: Container())
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
