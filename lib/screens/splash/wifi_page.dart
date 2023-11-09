4821eb5cfe36a8a454dd5d5eae5692cfacc7b6cb
Kimberly Markley <markleki@oregonstate.edu>
2023-11-09 11:52:37 -0800

    fix alignment and background of wifi_page

--- lib/screens/splash/wifi_page.dart
+++ lib/screens/splash/wifi_page.dart
@@ -9,20 +9,28 @@ class WifiPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Column(
-      children: [
-        WifiConfigurationWidget(changeScreen: changeScreen),
-        Container(
-          decoration: const BoxDecoration(
-            image: DecorationImage(
-                image: AssetImage("assets/images/background2.JPG"),
-                fit: BoxFit.cover),
-          ),
-          child: const Center(
-            child: Text("Wifi Page Content"),
+      body: Container(
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+              image: AssetImage("assets/images/background2.JPG"),
+              fit: BoxFit.cover),
+        ),
+        child: Center(
+          child: Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                WifiConfigurationWidget(changeScreen: changeScreen),
+                const Padding(
+                  padding: EdgeInsets.all(20.0),
+                  child: Text("Wifi Page Content"),
+                )
+              ],
+            ),
           ),
         ),
-      ],
-    ));
+      ),
+    );
   }
 }
