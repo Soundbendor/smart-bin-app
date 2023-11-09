318ddef12e8adf36733e74b4f9096bb645c287b6
Micah <stalberm@oregonstate.edu>
2023-11-09 07:49:00 -0800

    Switches page navigation to use indexedstack, fixes MainPage rendering after splash screens

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,7 @@
 import 'package:flutter/material.dart';
-// import 'package:waste_watchers/screens/main/detections_page.dart';
-// import 'package:waste_watchers/screens/main/home_page.dart';
-// import 'package:waste_watchers/screens/main/stats_page.dart';
-// import 'package:waste_watchers/screens/splash/screen.dart';
-// import 'package:waste_watchers/screens/splash/pages/wifi_page.dart';
 import 'package:waste_watchers/screens/main/screen.dart';
 import 'package:waste_watchers/screens/splash/screen.dart';
+import 'package:waste_watchers/screens/splash/wifi_page.dart';
 
 void main() {
   runApp(const WasteWatchersApp());
@@ -31,19 +27,24 @@ class MainPage extends StatefulWidget {
 
 class _MainPageState extends State<MainPage> {
   int index = 0;
-  bool isWifiConnected = false;
 
-  void _changeWifiConnected() {
+  void _changeScreen(int newIndex) {
     setState(() {
-      isWifiConnected = true;
+      index = newIndex;
     });
   }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        home: isWifiConnected
-            ? const MainScreen()
-            : SplashPage(changeWifiConnected: _changeWifiConnected));
+      home: IndexedStack(
+        index: index,
+        children: [
+          SplashPage(changeScreen: _changeScreen),
+          WifiPage(changeScreen: _changeScreen),
+          const MainScreen(),
+        ],
+      ),
+    );
   }
 }
