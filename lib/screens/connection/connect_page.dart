4208b8ad64bb3de363791a116f31f98e66d19d4f
Blake Pugh <91357046+bpugh5@users.noreply.github.com>
2023-11-10 12:15:22 -0800

    Create Splash Screen/Navigation, Wifi Credential Widget (#14)
    * Splash screen added to /lib/screens, looks pretty :), main updated to accommodate it being the first screen when you open the app.
    
    * Shrunk bin size on splash screen, updated pubspec.yaml with dependencies
    
    * add wifi config connection to splash page button [NOT WORKING]
    
    * Added WiFi page and route
    
    * Refactored file structure to accommodate for multiple pages, fixed kim's widget display issue, passing state up to main to display home or splash page based on if wifi is connected or not!
    
    * Switches page navigation to use indexedstack, fixes MainPage rendering after splash screens
    
    * Remove unused import
    
    * fix alignment and background of wifi_page
    
    * Connect page added, needs refactoring but looks good as a proof of concept
    
    * Slight modification, ignore
    
    ---------
    
    Co-authored-by: Kimberly Markley <markleki@oregonstate.edu>
    Co-authored-by: Micah <stalberm@oregonstate.edu>
    Co-authored-by: Blake <bario54321@gmail.com>

---
+++ lib/screens/connection/connect_page.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+
+class ConnectPage extends StatelessWidget {
+  const ConnectPage({super.key});
+
+  @override
+Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+              image: AssetImage("assets/images/background2.JPG"),
+              fit: BoxFit.cover),
+        ),
+        child: Column(
+          children: [
+            const Flexible(
+              flex: 1,
+              child: Center(
+                child: Text(
+                  "Connect to your Bin!",
+                  style: TextStyle(
+                    fontSize: 30,
+                    ),
+                  ),
+                
+              ),
+            ),
+            Flexible(
+              flex: 5,
+              child: ListView.builder(
+                itemCount: 8,
+                itemBuilder: (BuildContext context, int index) {
+                  return Card(
+                    margin: const EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(15.0)
+                      ),
+                    child: ListTile(
+                    leading: const Icon(Icons.wifi),
+                    title: Text("Bin # $index"),
+                    subtitle: const Text('Strength:'),
+                    trailing: const Icon(Icons.keyboard_arrow_right),
+                    onTap: () {
+                      // Add functionality for when a network is tapped
+                    }
+                    ),
+                  );
+                }),
+                
+              ),
+          ],
+        ),
+      ),
+    );
+  }
+}
