c7682956b387f9dde18574b85dcf42c0e3c48240
unknown <pughbl@oregonstate.edu>
2023-11-10 12:09:42 -0800

    Slight modification, ignore

--- lib/screens/connection/connect_page.dart
+++ lib/screens/connection/connect_page.dart
@@ -17,13 +17,19 @@ Widget build(BuildContext context) {
             const Flexible(
               flex: 1,
               child: Center(
-                child: Text("Connect to your Bin!"),
+                child: Text(
+                  "Connect to your Bin!",
+                  style: TextStyle(
+                    fontSize: 30,
+                    ),
+                  ),
                 
               ),
             ),
             Flexible(
+              flex: 5,
               child: ListView.builder(
-                itemCount: 5,
+                itemCount: 8,
                 itemBuilder: (BuildContext context, int index) {
                   return Card(
                     margin: const EdgeInsets.symmetric(vertical: 10, horizontal: 20),
@@ -32,7 +38,7 @@ Widget build(BuildContext context) {
                       ),
                     child: ListTile(
                     leading: const Icon(Icons.wifi),
-                    title: Text("Wifi # $index"),
+                    title: Text("Bin # $index"),
                     subtitle: const Text('Strength:'),
                     trailing: const Icon(Icons.keyboard_arrow_right),
                     onTap: () {
