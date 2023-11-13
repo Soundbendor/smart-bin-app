f5ea715e0f347a7895d870d63402e076480cfa5e
unknown <pughbl@oregonstate.edu>
2023-11-13 13:57:33 -0800

    Dynamic scanning for wifi networks, works on AndroidWifi

--- lib/screens/connection/connect_page.dart
+++ lib/screens/connection/connect_page.dart
@@ -1,7 +1,63 @@
 import 'package:flutter/material.dart';
+import 'package:wifi_scan/wifi_scan.dart';
 
-class ConnectPage extends StatelessWidget {
-  const ConnectPage({super.key});
+class ConnectPage extends StatefulWidget {
+  const ConnectPage({Key? key}) : super(key: key);
+
+  @override
+  State<ConnectPage> createState() => _ConnectPageState();
+}
+
+class _ConnectPageState extends State<ConnectPage> {
+  List<WiFiAccessPoint> wifiResults = [];
+
+  @override
+  void initState() {
+    super.initState();
+    _startScan();
+  }
+
+  Future<void> _startScan() async {
+    final canStartScan = await WiFiScan.instance.canStartScan(askPermissions: true);
+
+    switch (canStartScan) {
+    case CanStartScan.yes:
+      final isScanning = await WiFiScan.instance.startScan();
+      if (isScanning) {
+        _getScannedResults();
+      }
+      break;
+    case CanStartScan.failed:
+      // Handle the case where scanning is not possible
+      break;
+    case CanStartScan.notSupported:
+      // Handle the case where the user denied the necessary permissions
+      break;
+    default:
+      // handle default case
+  }
+}
+
+  Future<void> _getScannedResults() async {
+    final canGetResults = await WiFiScan.instance.canGetScannedResults(askPermissions: true);
+
+    switch (canGetResults) {
+      case CanGetScannedResults.yes:
+        final accessPoints = await WiFiScan.instance.getScannedResults();
+        setState(() {
+          wifiResults = accessPoints;
+        });
+        break;
+      case CanGetScannedResults.noLocationServiceDisabled:
+        // Handle the case where getting results is not possible
+        break;
+      case CanGetScannedResults.noLocationPermissionDenied:
+        // Handle the case where the user denied the necessary permissions
+        break;
+      default:
+      // handle default
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -28,8 +84,9 @@ class ConnectPage extends StatelessWidget {
             Flexible(
               flex: 5,
               child: ListView.builder(
-                  itemCount: 8,
+                  itemCount: wifiResults.length,
                   itemBuilder: (BuildContext context, int index) {
+                    final wifiResult = wifiResults[index];
                     return Card(
                       margin: const EdgeInsets.symmetric(
                           vertical: 10, horizontal: 20),
@@ -37,7 +94,7 @@ class ConnectPage extends StatelessWidget {
                           borderRadius: BorderRadius.circular(15.0)),
                       child: ListTile(
                           leading: const Icon(Icons.wifi),
-                          title: Text("Bin # $index"),
+                          title: Text(wifiResult.ssid),
                           subtitle: const Text('Strength:'),
                           trailing: const Icon(Icons.keyboard_arrow_right),
                           onTap: () {
