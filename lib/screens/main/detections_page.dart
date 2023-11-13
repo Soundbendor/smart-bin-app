3cfd0a3b6a2086cf5a8c9e1f13b8de9cc22ad4c7
Daniel Lau <32113157+theusaf@users.noreply.github.com>
2023-11-13 11:44:59 -0800

    Detections: Stub detection list page (#13)
    * Stub initial detection list widget
    
    * Add header and empty text to detection page
    
    * Add example UI elements to detection list
    
    - Renames the file to 'detections'

--- lib/screens/main/detections_page.dart
+++ lib/screens/main/detections_page.dart
@@ -1,13 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:waste_watchers/widgets/detections.dart';
+import 'package:waste_watchers/widgets/heading.dart';
 
 class DetectionsPage extends StatelessWidget {
   const DetectionsPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text("Detections Page Content"),
+    return Scaffold(
+      body: Container(
+        padding: const EdgeInsets.all(10),
+        child: const Column(
+          children: [
+            Heading(text: "Detections"),
+            DetectionList(
+              detections: [
+                DetectionListItem.stub(),
+                DetectionListItem.stub(),
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
