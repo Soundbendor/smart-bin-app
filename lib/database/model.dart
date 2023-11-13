38fa62ab1ec1838cb627bd5b46eb340185c6daff
Daniel Lau <32113157+theusaf@users.noreply.github.com>
2023-11-13 11:48:15 -0800

    Database: Create database connections and models (#12)
    * Database: Add dependencies and initial setup
    
    * Database: create models
    
    * Database: Prevent unneeded opening of database connections
    
    * Database: Add method to close connection
    
    * Database: Add database methods to classes
    
    - Save / Delete
    
    * Format files
    
    * Resolve database connection issues
    
    * Add debugging statements

---
+++ lib/database/model.dart
@@ -0,0 +1,16 @@
+import 'package:sqflite/sqflite.dart';
+import 'package:waste_watchers/database/connection.dart';
+
+abstract class Model {
+  String get tableName;
+  String get schema;
+  Map<String, dynamic> toMap();
+
+  Future<void> save() async {
+    Database db = await getDatabaseConnection();
+    await db.insert(tableName, toMap(),
+        conflictAlgorithm: ConflictAlgorithm.replace);
+  }
+
+  Future<void> delete();
+}
