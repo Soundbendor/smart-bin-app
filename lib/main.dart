83b3e9ecbfba01763975776b2bd11dd9a58331ba
stalberm <77121289+stalberm@users.noreply.github.com>
2023-11-20 11:45:37 -0800

    Rework to use go router (#22)
    * Splash screen added to /lib/screens, looks pretty :), main updated to accommodate it being the first screen when you open the app.
    
    * Shrunk bin size on splash screen, updated pubspec.yaml with dependencies
    
    * add wifi config connection to splash page button [NOT WORKING]
    
    * Added WiFi page and route
    
    * Refactored file structure to accommodate for multiple pages, fixed kim's widget display issue, passing state up to main to display home or splash page based on if wifi is connected or not!
    
    * Switches page navigation to use indexedstack, fixes MainPage rendering after splash screens
    
    * Remove unused import
    
    * fix alignment and background of wifi_page
    
    * Connect page added, needs refactoring but looks good as a proof of concept
    
    * Slight modification, ignore
    
    * Added connection page, refactoring list builder
    
    * fixed merge conflicts
    
    * Dynamic scanning for wifi networks, works on AndroidWifi
    
    * Clicking wifi button takes you to home screen
    
    * Start of regexing available networks, filtering by name and displaying only what we need
    
    * replace Waste Watchers with binsight.ai
    
    * Adds wifi connectivity from connect_page cards
    
    * Adds dialog_popup widget
    
    * replace indexed stacks with go_router
    
    * initial go route functionality added
    
    ---------
    
    Co-authored-by: unknown <pughbl@oregonstate.edu>
    Co-authored-by: Kimberly Markley <markleki@oregonstate.edu>
    Co-authored-by: Blake <bario54321@gmail.com>

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,17 @@
 import 'package:flutter/material.dart';
-import 'package:waste_watchers/database/connection.dart';
-import 'package:waste_watchers/screens/main/screen.dart';
+import 'package:waste_watchers/screens/main/detections_page.dart';
+import 'package:waste_watchers/screens/main/home_page.dart';
+import 'package:waste_watchers/screens/main/stats_page.dart';
 import 'package:waste_watchers/screens/splash/screen.dart';
 import 'package:waste_watchers/screens/splash/wifi_page.dart';
 import 'package:waste_watchers/screens/connection/connect_page.dart';
+import 'package:waste_watchers/database/connection.dart';
+import 'package:go_router/go_router.dart';
+
+final GlobalKey<NavigatorState> _rootNavigatorKey =
+    GlobalKey<NavigatorState>(debugLabel: 'root');
+final GlobalKey<NavigatorState> _shellNavigatorKey =
+    GlobalKey<NavigatorState>(debugLabel: 'shell');
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -16,40 +24,136 @@ class WasteWatchersApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: MainPage(),
+    return MaterialApp.router(
+      routerConfig: _router,
     );
   }
 }
 
-class MainPage extends StatefulWidget {
-  const MainPage({Key? key}) : super(key: key);
+final GoRouter _router = GoRouter(
+  initialLocation: '/set-up',
+  routes: <RouteBase>[
+    GoRoute(
+      path: '/',
+      builder: (BuildContext context, GoRouterState state) {
+        return Container();
+      },
+      routes: <RouteBase>[
+        ShellRoute(
+          builder: (BuildContext context, GoRouterState state, Widget child) {
+            return BottomNavBar(child: child);
+          },
+          routes: <GoRoute>[
+            GoRoute(
+                name: 'main',
+                path: 'main',
+                builder: (BuildContext context, GoRouterState state) {
+                  return const HomePage();
+                },
+                routes: [
+                  GoRoute(
+                    name: 'detections',
+                    path: 'detections',
+                    builder: (BuildContext context, GoRouterState state) {
+                      return const DetectionsPage();
+                    },
+                  ),
+                  GoRoute(
+                    name: 'stats',
+                    path: 'stats',
+                    builder: (BuildContext context, GoRouterState state) {
+                      return const StatsPage();
+                    },
+                  ),
+                ]),
+          ],
+        )
+      ],
+    ),
+    GoRoute(
+        name: 'set-up',
+        path: '/set-up',
+        builder: (BuildContext conext, GoRouterState state) {
+          return const SplashPage();
+        },
+        routes: [
+          GoRoute(
+              name: 'wifi',
+              path: 'wifi',
+              builder: (BuildContext context, GoRouterState state) {
+                return const WifiPage();
+              }),
+          GoRoute(
+              name: 'bin_connect',
+              path: 'bin_connect',
+              builder: (BuildContext context, GoRouterState state) {
+                return const ConnectPage();
+              })
+        ]),
+  ],
+);
 
-  @override
-  State<MainPage> createState() => _MainPageState();
-}
+class BottomNavBar extends StatelessWidget {
+  const BottomNavBar({
+    required this.child,
+    Key? key,
+  }) : super(key: key);
 
-class _MainPageState extends State<MainPage> {
-  int index = 0;
-
-  void _changeScreen(int newIndex) {
-    setState(() {
-      index = newIndex;
-    });
-  }
+  final Widget child;
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: IndexedStack(
-        index: index,
-        children: [
-          SplashPage(changeScreen: _changeScreen),
-          WifiPage(changeScreen: _changeScreen),
-          const ConnectPage(),
-          const MainScreen(),
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Waste Watchers"),
+        centerTitle: true,
+      ),
+      body: child,
+      bottomNavigationBar: BottomNavigationBar(
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.search),
+            label: 'Detections',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.show_chart),
+            label: 'Stats',
+          ),
         ],
+        currentIndex: _calculateSelectedIndex(context),
+        onTap: (int idx) => _onItemTapped(idx, context),
       ),
     );
+  }
+
+  static int _calculateSelectedIndex(BuildContext context) {
+    final String location = GoRouterState.of(context).uri.toString();
+    if (location == '/main') {
+      return 0;
+    }
+    if (location == '/main/detections') {
+      return 1;
+    }
+    if (location == '/main/stats') {
+      return 2;
+    }
+    return 0;
+  }
+
+  void _onItemTapped(int index, BuildContext context) {
+    switch (index) {
+      case 0:
+        GoRouter.of(context).go('/main');
+        break;
+      case 1:
+        GoRouter.of(context).go('/main/detections');
+        break;
+      case 2:
+        GoRouter.of(context).go('/main/stats');
+    }
   }
 }
