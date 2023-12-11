9e320d0a30e338f32dcc6b3a54652236b34218e3
stalberm <77121289+stalberm@users.noreply.github.com>
2023-12-11 11:38:32 -0800

    Pub Sub: Implement initial incorporation (#34)
    * Implement subscription message processing and database interaction
    
    * Adds stubbed device id initial sub message
    
    * Changes recieved message handler function name
    
    * Fixes subscription route and postDetection handling
    
    * Updates database version for model change

--- lib/pub_sub/subscriber.dart
+++ lib/pub_sub/subscriber.dart
@@ -1,10 +1,64 @@
-import 'package:flutter/material.dart';
+import 'package:binsight_ai/database/connection.dart';
+import 'package:binsight_ai/database/models/detection.dart';
+import 'package:web_socket_channel/web_socket_channel.dart';
+import 'package:sqflite/sqflite.dart';
+import 'dart:convert';
 
-// Notifies the app that the database has changed
-class DatabaseNotifier extends ChangeNotifier {
-  void notify() {
-    notifyListeners();
-  }
+void handleMessages(WebSocketChannel channel, Database database) {
+  channel.stream.listen(
+    (data) async {
+      final jsonData = jsonDecode(data);
+      final messageType = jsonData['type'];
+      if (messageType == 'pre_detection') {
+        await updatePreDetection(jsonData["pre_detection"], database);
+      } else if (messageType == 'post_detection') {
+        await updatePostDetection(jsonData["post_detection"], database);
+      }
+    },
+    onDone: () {
+      print('Socket Closed');
+    },
+    onError: (error) {
+      print('Socket Error: $error');
+    },
+  );
 }
 
-final detectionNotifier = DatabaseNotifier();
+Future<void> updatePreDetection(
+    Map<String, dynamic> data, Database database) async {
+  Detection detection = Detection(
+      imageId: data['img_id'],
+      preDetectImgLink: data['img_link'],
+      timestamp: DateTime.now(),
+      deviceId: "device_id",
+      depthMapImgLink: data['depth_map_link'],
+      irImgLink: data['ir_link'], //Update name to name in pydantic model
+      weight: data['weight'],
+      humidity: data['humditiy'],
+      temperature: data['temperature'],
+      co2: data['co2'], //Update name to name in pydantic model
+      vo2: data['vo2'], //Update name to name in pydantic model
+      boxes: data['boxes'] //Update name to name in pydantic model
+      );
+
+  database = await getDatabaseConnection();
+  await database.insert(detection.tableName, detection.toMap());
+}
+
+Future<void> updatePostDetection(
+    Map<String, dynamic> data, Database database) async {
+  List<Map<String, dynamic>> detections = await database.query(
+    "detections",
+    where: "img_id = ?",
+    whereArgs: [data['img_id']],
+  );
+  Map<String, dynamic> detection = detections[0];
+  if (detection.isNotEmpty) {
+    await database.update(
+      "detections",
+      {'postDetectImgLink': data['img_link']},
+      where: "img_id = ?",
+      whereArgs: [data['img_id']],
+    );
+  }
+}
