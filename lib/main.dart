e050344dddcbc31d14ec69a9012d5d220b3d7928
theusaf <theusafyt@gmail.com>
2024-01-13 16:26:22 -0800

    Add splash screen
    Currently, image is very squished, need a better one
    Proof of concept works though

--- lib/main.dart
+++ lib/main.dart
@@ -15,8 +15,8 @@ import 'package:web_socket_channel/io.dart';
 import 'dart:convert';
 
 void main() async {
-  // WidgetsFlutterBinding.ensureInitialized();
-  // final Database database = await getDatabaseConnection();
+  WidgetsFlutterBinding.ensureInitialized();
+  final Database database = await getDatabaseConnection();
   // final channel =
   //     IOWebSocketChannel.connect('http://54.214.80.15/api/model/subscribe');
   // final subscriptionMessage = {
