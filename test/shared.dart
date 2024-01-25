b405c681941bf69b8ce942036d4ed71983938994
theusaf <theusafyt@gmail.com>
2024-01-24 18:12:59 -0800

    Test/Detection: Check title content
    - ignores overflow errors

--- test/shared.dart
+++ test/shared.dart
@@ -14,6 +14,34 @@ Widget makeTestableWidget({required Widget child, required Size size}) {
   );
 }
 
+// Taken from https://remelehane.dev/posts/widget-testing-rendeflex-overflow/
+/// Ignores overflow errors in tests.
+void ignoreOverflowErrors(
+  FlutterErrorDetails details, {
+  bool forceReport = false,
+}) {
+  bool ifIsOverflowError = false;
+  bool isUnableToLoadAsset = false;
+
+  // Detect overflow error.
+  var exception = details.exception;
+  if (exception is FlutterError) {
+    ifIsOverflowError = !exception.diagnostics.any(
+      (e) => e.value.toString().startsWith("A RenderFlex overflowed by"),
+    );
+    isUnableToLoadAsset = !exception.diagnostics.any(
+      (e) => e.value.toString().startsWith("Unable to load asset"),
+    );
+  }
+
+  // Ignore if is overflow error.
+  if (ifIsOverflowError || isUnableToLoadAsset) {
+    debugPrint('Ignored Error');
+  } else {
+    FlutterError.dumpErrorToConsole(details, forceReport: forceReport);
+  }
+}
+
 class FakeDatabase implements Database {
   bool isClosed = false;
   final List<String> queries = [];
