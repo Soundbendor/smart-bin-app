16d28daefe01923d425021598f9c929f87464885
Kimberly Markley <markleki@oregonstate.edu>
2024-01-24 18:45:59 -0800

    create drawer style TopNavBar

--- lib/ui_components/top_nav_bar.dart
+++ lib/ui_components/top_nav_bar.dart
@@ -1,66 +1,103 @@
 import 'package:flutter/material.dart';
-import 'package:go_router/go_router.dart';
 
 /// Top navigation bar widget burger menu with slide out drawer functionality.
-class TopNavBar extends StatelessWidget {
+class TopNavBar extends StatefulWidget {
   const TopNavBar({
-    required this.child,
     super.key,
   });
 
-  final Widget child;
+  @override
+  State<TopNavBar> createState() => _TopNavBarState();
+}
+
+/// State class for the top navigation bar widget.
+class _TopNavBarState extends State<TopNavBar> {
+
+  // Set default selection in the navigation bar to the home page.
+  int _selectedIndex = 0;
+
+  static const TextStyle optionStyle =
+      TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
+  static const List<Widget> _widgetOptions = <Widget>[
+    Text(
+      'Index 0: Home',
+      style: optionStyle,
+    ),
+    Text(
+      'Index 1: Profile',
+      style: optionStyle,
+    ),
+    Text(
+      'Index 2: Help',
+      style: optionStyle,
+    ),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: child,
-      topNavigationBar: TopNavigationBar(
-        items: const [
-          TopNavigationBarItem(
-            icon: Icon(Icons.home),
-            label: 'Home',
-          ),
-          TopNavigationBarItem(
-            icon: Icon(Icons.search),
-            label: 'Detections',
-          ),
-          TopNavigationBarItem(
-            icon: Icon(Icons.show_chart),
-            label: 'Stats',
-          ),
-        ],
-        currentIndex: _calculateSelectedIndex(context),
-        onTap: (int idx) => _onItemTapped(idx, context),
+      appBar: AppBar(
+        title: const Text('Binsight.ai'),
+        ),
+      // When user opens the drawer, it is added to the navigation stack.
+      drawer: Drawer(
+        // Add a ListView to the drawer. This ensures the user can scroll
+        // through the options in the drawer if there isn't enough vertical
+        // space to fit everything.
+        child: ListView(
+          // Important: Remove any padding from the ListView.
+          padding: EdgeInsets.zero,
+          children: [
+            const DrawerHeader(
+              decoration: BoxDecoration(
+                color: Colors.blue,
+              ),
+              child: Text(
+                'Binsight.ai',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 24,
+                ),
+              ),
+            ),
+            ListTile(
+              leading: const Icon(Icons.home),
+              title: const Text('Home'),
+              selected: _selectedIndex == 0,
+              onTap: () {
+                // Update the state of the app.
+                _onItemTapped(0);
+                // Then close the drawer.
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              leading: const Icon(Icons.person_2_outlined),
+              title: const Text('Profile'),
+              selected: _selectedIndex == 1,
+              onTap: () {
+                _onItemTapped(1);
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              leading: const Icon(Icons.question_mark_outlined),
+              title: const Text('Help'),
+              selected: _selectedIndex == 2,
+              onTap: () {
+                _onItemTapped(2);
+                Navigator.pop(context);
+              },
+            ),
+          ],
+        ),
       ),
     );
-  }
-
-  // Calculate the index of the bottom navigation bar based on the current route
-  static int _calculateSelectedIndex(BuildContext context) {
-    final String location = GoRouterState.of(context).uri.toString();
-    if (location == '/main') {
-      return 0;
-    }
-    if (location == '/main/detections') {
-      return 1;
-    }
-    if (location == '/main/stats') {
-      return 2;
-    }
-    return 0;
-  }
-
-  // Function to handle navigation when an item is tapped
-  void _onItemTapped(int index, BuildContext context) {
-    switch (index) {
-      case 0:
-        GoRouter.of(context).go('/main');
-        break;
-      case 1:
-        GoRouter.of(context).go('/main/detections');
-        break;
-      case 2:
-        GoRouter.of(context).go('/main/stats');
-    }
   }
 }
