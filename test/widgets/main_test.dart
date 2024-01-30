3ef0011f308b1b3ee48f700378b4021784aeae2f
theusaf <theusafyt@gmail.com>
2024-01-30 15:08:42 -0800

    Update tests
    Ignores rendering issues, since it's not testing that

--- test/widgets/main_test.dart
+++ test/widgets/main_test.dart
@@ -4,9 +4,13 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:go_router/go_router.dart';
 
-void main() {
+import '../shared.dart';
 
-  testWidgets("Initial location is at set-up when devices don't exist", (widgetTester) async {
+void main() {
+  testWidgets("Initial location is at set-up when devices don't exist",
+      (widgetTester) async {
+    final originalErrorHandler = FlutterError.onError;
+    FlutterError.onError = ignoreOverflowErrors(originalErrorHandler);
     setRoutes([
       GoRoute(
         name: 'set-up',
@@ -24,14 +28,16 @@ void main() {
       ),
     ]);
 
-    await widgetTester.pumpWidget(const BinsightAiApp(skipSetUp: false));
-    expect(
-      router.routerDelegate.currentConfiguration.last.matchedLocation,
-      equals("/set-up")
-    );
+    await widgetTester.pumpWidget(makeTestableWidget(
+        child: const BinsightAiApp(skipSetUp: false),
+        size: const Size(800, 600)));
+    expect(router.routerDelegate.currentConfiguration.last.matchedLocation,
+        equals("/set-up"));
+    FlutterError.onError = originalErrorHandler;
   });
 
-  testWidgets("Initial location is at main when devices exist", (widgetTester) async {
+  testWidgets("Initial location is at main when devices exist",
+      (widgetTester) async {
     setRoutes([
       GoRoute(
         name: 'set-up',
@@ -50,10 +56,7 @@ void main() {
     ]);
 
     await widgetTester.pumpWidget(const BinsightAiApp(skipSetUp: true));
-    expect(
-      router.routerDelegate.currentConfiguration.last.matchedLocation,
-      equals("/main")
-    );
+    expect(router.routerDelegate.currentConfiguration.last.matchedLocation,
+        equals("/main"));
   });
-
 }
