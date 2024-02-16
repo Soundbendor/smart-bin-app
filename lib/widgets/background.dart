91e110b5a60dd29e0ae9adf0f3715a579ec549b5
unknown <pughbl@oregonstate.edu>
2024-02-16 15:10:03 -0800

    Add documentation to background.dart

--- lib/widgets/background.dart
+++ lib/widgets/background.dart
@@ -1,5 +1,10 @@
 import 'package:flutter/material.dart';
 
+/// Creates a decorated container with a background and child specified by the user using the imageURL and child properties (respectively).
+/// 
+/// This will typically be used as the body of a Scaffold to apply a background image to the page it is on.
+/// 
+/// If no imageURL is specified, it defaults to "background2.JPG".
 class CustomBackground extends StatelessWidget {
   const CustomBackground(
       {super.key,
