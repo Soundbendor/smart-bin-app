0f465f1e22e1dc50edd1ae07413e9b0b72cd3536
theusaf <theusafyt@gmail.com>
2024-02-16 22:28:32 -0800

    Remove wifi-scan related dependencies

--- lib/pages/setup/wifi_scan.dart
+++ lib/pages/setup/wifi_scan.dart
@@ -1,10 +1,7 @@
 import 'dart:convert';
 import 'package:flutter/material.dart';
-import 'package:flutter_reactive_ble/flutter_reactive_ble.dart';
 import 'package:go_router/go_router.dart';
 import 'package:provider/provider.dart';
-import 'package:wifi_iot/wifi_iot.dart';
-import 'package:wifi_scan/wifi_scan.dart';
 import 'package:binsight_ai/util/print.dart';
 import 'package:binsight_ai/util/bluetooth.dart';
 import 'package:binsight_ai/util/providers.dart';
@@ -20,91 +17,10 @@ class WifiScanPage extends StatefulWidget {
 class _WifiScanPageState extends State<WifiScanPage> {
   // _WifiScanPageState();
 
-  List<WiFiAccessPoint> wifiResults = [];
-  final flutterReactiveBle = FlutterReactiveBle();
-  final Uuid _binServiceID = Uuid.parse("31415924535897932384626433832790");
-  final Uuid _wifiCredID = Uuid.parse("31415924535897932384626433832793");
-
-  Future<void> _initializeWifiPlugin() async {
-    await WiFiForIoTPlugin.forceWifiUsage(true);
-  }
+  List<String> wifiResults = [];
 
   void _startScan(BleDevice? bluetoothDevice) {
-    if (bluetoothDevice != null) {
-      final characteristic = QualifiedCharacteristic(
-          serviceId: _binServiceID,
-          characteristicId: _wifiCredID,
-          deviceId: bluetoothDevice.id);
-      flutterReactiveBle.subscribeToCharacteristic(characteristic).listen(
-        (data) {
-          debug(utf8.decode(data));
-          // }, onError: (dynamic error) {
-        },
-      );
-    }
-  }
-
-  // Future<void> _startScan() async {
-  //   final canStartScan =
-  //       await WiFiScan.instance.canStartScan(askPermissions: true);
-  //   // Switch statement should go here
-  //   switch (canStartScan) {
-  //     case CanStartScan.yes:
-  //       final isScanning = await WiFiScan.instance.startScan();
-  //       if (isScanning) {
-  //         _getScannedResults();
-  //       }
-  //       break;
-  //     case CanStartScan.failed:
-  //       // Handle the case where scanning is not possible
-  //       break;
-  //     case CanStartScan.notSupported:
-  //       // Handle the case where the user denied the necessary permissions
-  //       break;
-  //     case CanStartScan.noLocationServiceDisabled:
-  //       debug("location service disabled");
-  //     default:
-  //       // handle default case
-  //       debug("default case: $canStartScan");
-  //   }
-  // }
-
-  Future<void> _getScannedResults() async {
-    final canGetResults =
-        await WiFiScan.instance.canGetScannedResults(askPermissions: true);
-
-    switch (canGetResults) {
-      case CanGetScannedResults.yes:
-        final accessPoints = await WiFiScan.instance.getScannedResults();
-        // Define regex on wifi ssid
-        RegExp wifiNameCheck = RegExp(r'');
-        // List of wifi access points that will store the filtered, regex'd networks that match our bins
-        List<WiFiAccessPoint> filteredAccessPoints = [];
-        // List of strings that keep track of what access points have been added to our display list
-        List<String> filteredAccessPointsString = [];
-        // Iterate through all scanned access points and only add the ones that meet our criteria
-        for (var point in accessPoints) {
-          if (wifiNameCheck.hasMatch(point.ssid) &&
-              !filteredAccessPointsString.contains(point.ssid)) {
-            filteredAccessPoints.add(point);
-          }
-
-          filteredAccessPointsString.add(point.ssid);
-        }
-
-        setState(() {
-          wifiResults = filteredAccessPoints;
-        });
-        break;
-      case CanGetScannedResults.noLocationServiceDisabled:
-        // Handle the case where getting results is not possible
-        break;
-      case CanGetScannedResults.noLocationPermissionDenied:
-        // Handle the case where the user denied the necessary permissions
-        break;
-      default:
-      // handle default
-    }
+    // TODO: implement scan
   }
 
   @override
@@ -162,11 +78,11 @@ class _WifiScanPageState extends State<WifiScanPage> {
                           borderRadius: BorderRadius.circular(15.0)),
                       child: ListTile(
                           leading: const Icon(Icons.wifi),
-                          title: Text(wifiResult.ssid),
+                          title: Text(wifiResult),
                           trailing: const Icon(Icons.keyboard_arrow_right),
                           onTap: () async {
                             GoRouter.of(context)
-                                .goNamed('wifi', extra: wifiResult.ssid);
+                                .goNamed('wifi', extra: wifiResult);
                           }),
                     );
                   },
