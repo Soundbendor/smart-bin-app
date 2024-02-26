65d50b89329e86fd47f230a550d2d16ec0a1e053
Kimberly Markley <markleki@oregonstate.edu>
2024-02-26 14:56:49 -0800

    bug squash: removed Expanded widgets from inside ExpansionTiles

--- lib/pages/main/help.dart
+++ lib/pages/main/help.dart
@@ -26,82 +26,73 @@ class HelpPage extends StatelessWidget {
         padding: const EdgeInsets.all(10),
         child: ListView(
           children: [
-            Expanded(
-              child: ExpansionTile(
-                shape: Border.all(color: Colors.transparent),
-                title: const Heading(text: "FAQ"),
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Text("FAQ Content", style: textTheme),
-                  ),
-                ],
-              ),
+            ExpansionTile(
+              shape: Border.all(color: Colors.transparent),
+              title: const Heading(text: "FAQ"),
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Text("FAQ Content", style: textTheme),
+                ),
+              ],
             ),
-            Expanded(
-              child: ExpansionTile(
-                shape: Border.all(color: Colors.transparent),
-                title: const Heading(text: "User Guide"),
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Text("User Guide Content", style: textTheme),
-                  ),
-                ],
-              ),
+            ExpansionTile(
+              shape: Border.all(color: Colors.transparent),
+              title: const Heading(text: "User Guide"),
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Text("User Guide Content", style: textTheme),
+                ),
+              ],
             ),
-            Expanded(
-              child: ExpansionTile(
-                shape: Border.all(color: Colors.transparent),
-                title: const Heading(text: "Help"),
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Text("Help Content", style: textTheme),
-                  ),
-                ],
-              ),
+            ExpansionTile(
+              shape: Border.all(color: Colors.transparent),
+              title: const Heading(text: "Help"),
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Text("Help Content", style: textTheme),
+                ),
+              ],
             ),
-            Expanded(
-              child: ExpansionTile(
-                shape: Border.all(color: Colors.transparent),
-                title: const Heading(text: "Contact Us"),
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Column(
-                      children: [
-                        const Text(
-                            "Have questions or need help with your bin?\nContact us for help!"),
-                        const SizedBox(height: 10.0),
-                        Padding(
-                          padding: const EdgeInsets.symmetric(horizontal: 8.0),
-                          child: TextButton(
-                            onPressed: _launchEmail,
-                            child: const Row(
-                              mainAxisSize: MainAxisSize.min,
-                              children: [
-                                Icon(
-                                  Icons.email,
-                                  color: Color.fromARGB(255, 33, 63, 148),
-                                  size: 30,
-                                ),
-                                SizedBox(width: 18.0),
-                                Text('Email',
-                                    style: TextStyle(
-                                        color:
-                                            Color.fromARGB(255, 255, 255, 255),
-                                        fontSize: 18.0,
-                                        fontWeight: FontWeight.w500)),
-                              ],
-                            ),
+            ExpansionTile(
+              shape: Border.all(color: Colors.transparent),
+              title: const Heading(text: "Contact Us"),
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Column(
+                    children: [
+                      const Text(
+                          "Have questions or need help with your bin?\nContact us for help!"),
+                      const SizedBox(height: 10.0),
+                      Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                        child: TextButton(
+                          onPressed: _launchEmail,
+                          child: const Row(
+                            mainAxisSize: MainAxisSize.min,
+                            children: [
+                              Icon(
+                                Icons.email,
+                                color: Color.fromARGB(255, 33, 63, 148),
+                                size: 30,
+                              ),
+                              SizedBox(width: 18.0),
+                              Text('Email',
+                                  style: TextStyle(
+                                      color: Color.fromARGB(255, 255, 255, 255),
+                                      fontSize: 18.0,
+                                      fontWeight: FontWeight.w500)),
+                            ],
                           ),
                         ),
-                      ],
-                    ),
+                      ),
+                    ],
                   ),
-                ],
-              ),
+                ),
+              ],
             ),
           ],
         ),
