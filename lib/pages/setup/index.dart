e23d23773e67e897344c1f689e4df1bb4d62501f
Kimberly Markley <73560521+kimberlymarkley@users.noreply.github.com>
2024-03-08 15:00:24 -0800

    UI | Test new assets, colors, fonts (#124)
    * add project fonts Georama and GolosText
    
    * change background screen with test image
    
    * update pubspec.yaml file with correct font filenames
    
    * update placeholder background images for test
    
    * update styles file with custom colors

--- lib/pages/setup/index.dart
+++ lib/pages/setup/index.dart
@@ -12,7 +12,7 @@ class SplashPage extends StatelessWidget {
     final colorScheme = Theme.of(context).colorScheme;
     return Scaffold(
       body: CustomBackground(
-        imageURL: "assets/images/background3.JPG",
+        imageURL: "assets/images/welcome.png",
         child: Center(
           child: Column(
             children: [
@@ -25,14 +25,14 @@ class SplashPage extends StatelessWidget {
                         padding: const EdgeInsets.only(
                             left: 16, right: 16, bottom: 30, top: 160),
                         child: Text(
-                          "Welcome!",
+                          "Welcome",
                           style: textTheme.displayLarge,
                         )),
                     Padding(
                       padding: const EdgeInsets.only(
                           left: 80, right: 80, top: 20, bottom: 20),
                       child: Text(
-                        "Let's get you connected to your bin.",
+                        "Let's get you connected to your bin!",
                         style: textTheme.headlineLarge!.copyWith(
                           color: colorScheme.onBackground,
                         ),
