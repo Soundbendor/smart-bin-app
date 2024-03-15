5f2333fe0d5944d96ee51bff3baa100e6def1dc1
Kimberly Markley <markleki@oregonstate.edu>
2024-03-15 14:45:40 -0700

    add bee element to header, add image to Tap to Annotate Latest Image, adjust layout and element sizing

--- lib/pages/main/home.dart
+++ lib/pages/main/home.dart
@@ -13,8 +13,10 @@ class HomePage extends StatefulWidget {
   State<HomePage> createState() => _HomePageState();
 }
 
-/// The home page of the application, displaying the user's latest image detection
-/// and a visual summary of their detections.
+/// The home page of the application
+///
+/// Contains an invitation to the user to annotate the latest detection
+/// and a visual summary of all detections so far, as a circular chart and a bar graph.
 class _HomePageState extends State<HomePage> {
   // All detections
   List<Detection> detections = [];
@@ -24,7 +26,7 @@ class _HomePageState extends State<HomePage> {
   Map<DateTime, double> weightCounts = {};
   late Future loadDetectionFuture;
 
-  // Load all the detections upon building
+  // Load all detections from the database
   @override
   void initState() {
     loadDetectionFuture = Detection.all().then((value) async {
@@ -35,13 +37,13 @@ class _HomePageState extends State<HomePage> {
     super.initState();
   }
 
+  // Build the home page
   @override
   Widget build(BuildContext context) {
-    final textTheme = Theme.of(context).textTheme;
-    final colorScheme = Theme.of(context).colorScheme;
-    
+    // Populate the counts for the circular chart and bar graph
     populateCounts();
-    
+
+    // Get the latest image detection from the compost bin
     Detection? latest;
     if (detections.isNotEmpty) {
       latest = detections[0];
@@ -51,41 +53,55 @@ class _HomePageState extends State<HomePage> {
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          SizedBox(height: 20),
+          const SizedBox(height: 20),
           Padding(
             padding: const EdgeInsets.symmetric(horizontal: 16),
-            child: Text(
-              "Review",
-              style: Theme.of(context).textTheme.displayMedium,
-            ),
-          ),
-                    SizedBox(height: 10),
-          if (latest != null) ...[
-            GestureDetector(
-              onTap: () => GoRouter.of(context)
-                  .push("/main/detection/${latest!.imageId}"),
-              child: Card(
-                child: Padding(
-                  padding: const EdgeInsets.all(16.0),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.stretch,
-                    children: [
-                      Placeholder(
-                        fallbackHeight: 200,
-                        color: Colors.grey,
-                      ),
-                      SizedBox(height: 10),
-                      Text(
-                        "Tap to Annotate Latest Image",
-                        textAlign: TextAlign.center,
-                        style: Theme.of(context).textTheme.bodyMedium,
-                      ),
-                    ],
+            child: Row(
+              children: [
+                Expanded(
+                  child: Text(
+                    "Review",
+                    style: Theme.of(context).textTheme.displayMedium,
                   ),
                 ),
+                Expanded(
+                  child: Image.asset(
+                    'assets/images/transparent_bee.png',
+                    fit: BoxFit.contain,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          const SizedBox(height: 10),
+          Card(
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  Text(
+                    "Tap to Annotate Latest Image",
+                    style: Theme.of(context).textTheme.titleMedium,
+                  ),
+                  const SizedBox(height: 8),
+                  latest != null
+                      ? GestureDetector(
+                          onTap: () => GoRouter.of(context)
+                              .push("/main/detection/${latest!.imageId}"),
+                          child: Image.asset(
+                            'assets/images/header_compost.png',
+                            fit: BoxFit.cover,
+                            height: 200,
+                          ),
+                        )
+                      : Container(), // Container to handle the case when latest is null
+                  const SizedBox(height: 10),
+                ],
               ),
             ),
-          ],
+          ),
+          const SizedBox(height: 20),
           Padding(
             padding: const EdgeInsets.symmetric(horizontal: 16),
             child: Text(
@@ -93,7 +109,7 @@ class _HomePageState extends State<HomePage> {
               style: Theme.of(context).textTheme.displayMedium,
             ),
           ),
-          SizedBox(height: 10),
+          const SizedBox(height: 10),
           Card(
             child: Padding(
               padding: const EdgeInsets.all(16.0),
@@ -104,7 +120,7 @@ class _HomePageState extends State<HomePage> {
                     "Detections by Food Category",
                     style: Theme.of(context).textTheme.titleMedium,
                   ),
-                  SizedBox(height: 10),
+                  const SizedBox(height: 10),
                   CircleChart(
                     data: labelCounts,
                   ),
@@ -112,7 +128,7 @@ class _HomePageState extends State<HomePage> {
               ),
             ),
           ),
-          SizedBox(height: 20),
+          const SizedBox(height: 20),
           Padding(
             padding: const EdgeInsets.symmetric(horizontal: 16),
             child: Text(
@@ -120,7 +136,7 @@ class _HomePageState extends State<HomePage> {
               style: Theme.of(context).textTheme.displayMedium,
             ),
           ),
-          SizedBox(height: 10),
+          const SizedBox(height: 10),
           Card(
             child: Padding(
               padding: const EdgeInsets.all(16.0),
@@ -131,7 +147,7 @@ class _HomePageState extends State<HomePage> {
                     "Compost Over Time",
                     style: Theme.of(context).textTheme.titleMedium,
                   ),
-                  SizedBox(height: 10),
+                  const SizedBox(height: 10),
                   FractionallySizedBox(
                     widthFactor: 0.9,
                     child: LineChart(
@@ -142,14 +158,15 @@ class _HomePageState extends State<HomePage> {
               ),
             ),
           ),
-          SizedBox(height: 20),
+          const SizedBox(height: 20),
         ],
       ),
     );
   }
 
-  /// Function to populate the weightCounts and labelCounts
+  /// Function to populate the weightCounts and labelCounts for the circular chart and bar graph
   void populateCounts() {
+    // If there are detections
     if (detections.isNotEmpty) {
       // Turn detections to a list of maps
       List<Map<String, dynamic>> detectionsMaps =
@@ -173,33 +190,5 @@ class _HomePageState extends State<HomePage> {
         }
       }
     }
-  }
-}
-
-/// Button to navigate to the latest detection's page
-class LabelButton extends StatelessWidget {
-  const LabelButton({
-    super.key,
-    required this.detection,
-    required this.text,
-  });
-
-  final String text;
-  final Detection? detection;
-
-  @override
-  Widget build(BuildContext context) {
-    return FractionallySizedBox(
-      widthFactor: .75,
-      child: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: TextButton.icon(
-          icon: const Icon(Icons.edit_outlined),
-          onPressed: () => GoRouter.of(context)
-              .push("/main/detection/${detection!.imageId}"),
-          label: Text(text),
-        ),
-      ),
-    );
   }
 }
