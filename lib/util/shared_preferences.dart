c8b879a17cf95844bdf1adeec6e387992e302c16
theusaf <theusafyt@gmail.com>
2024-05-05 14:07:18 -0700

    Document shared preference keys

--- lib/util/shared_preferences.dart
+++ lib/util/shared_preferences.dart
@@ -10,7 +10,19 @@ Future<void> initPreferences() async {
 
 /// Keys for the SharedPreferences.
 class SharedPreferencesKeys {
+  /// Whether to show the annotation tutorial.
   static const String dontShowAgain = 'dontShowAgain';
+
+  /// The device ID of the connected device.
+  ///
+  /// Used to reconnect to the device if needed.
   static const String deviceID = 'deviceID';
+
+  /// The API key for the backend API.
   static const String apiKey = 'compostApiKey';
+
+  /// The identifier for the device used in the API.
+  ///
+  /// Do not confuse this with the device ID used for Bluetooth.
+  static const String deviceApiID = 'deviceApiID';
 }
