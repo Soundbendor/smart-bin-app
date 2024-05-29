a3e1d24df24b3440b91761524f98f3739735fc1b
Blake <bario54321@gmail.com>
2024-05-29 13:43:25 -0700

    Fix detection card color issues that were lost

--- lib/widgets/detections.dart
+++ lib/widgets/detections.dart
@@ -73,7 +73,6 @@ class DetectionLargeListItem extends StatelessWidget {
                 Center(
                   child: Container(
                     width: 325,
-                    color: (jsonDecode(detection.boxes ?? "[]").isNotEmpty) ? mainColorScheme.tertiary : null,
                     margin: const EdgeInsets.only(bottom: 12, top: 12),
                     alignment: Alignment.center,
                     child: Stack(
@@ -154,7 +153,6 @@ class DetectionSmallListItem extends StatelessWidget {
         child: Card(
           color: ((jsonDecode(detection.boxes ?? "[]") as List).isNotEmpty) ? mainColorScheme.tertiary : colorScheme.onPrimary,
           child: ListTile(
-            tileColor: ((jsonDecode(detection.boxes ?? "[]") as List).isNotEmpty) ? mainColorScheme.tertiary : null,
             leading: Container(
               width: 50,
               height: 50,
