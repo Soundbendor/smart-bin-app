958fb9ce19259154f8393e277bc08f9f75e2699c
Blake <bario54321@gmail.com>
2024-05-29 10:42:56 -0700

    Fix background color of detection cards

--- lib/widgets/detections.dart
+++ lib/widgets/detections.dart
@@ -152,9 +152,8 @@ class DetectionSmallListItem extends StatelessWidget {
       child: GestureDetector(
         onTap: () => onTileTap(context, detection),
         child: Card(
-          color: colorScheme.onPrimary,
+          color: ((jsonDecode(detection.boxes ?? "[]") as List).isNotEmpty) ? mainColorScheme.tertiary :colorScheme.onPrimary,
           child: ListTile(
-            tileColor: ((jsonDecode(detection.boxes ?? "[]") as List).isNotEmpty) ? mainColorScheme.tertiary : null,
             leading: Container(
               width: 50,
               height: 50,
