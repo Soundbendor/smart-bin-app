367e3a14084fa39be53ffe4fcfc315eca9450612
Blake Pugh <91357046+bpugh5@users.noreply.github.com>
2024-05-29 13:11:23 -0700

    Annotated UI large card update (#243)
    * Update detections list card and detections page annotate button to be green when the detection has been annotated
    
    * Update annotated button to have checkmark rather than edit icon when detection has been annotated
    
    * Fix background color of detection cards
    
    * Add spacing
    
    * Update large card background color for detections
    
    ---------
    
    Co-authored-by: Blake <bario54321@gmail.com>

--- lib/widgets/detections.dart
+++ lib/widgets/detections.dart
@@ -55,7 +55,7 @@ class DetectionLargeListItem extends StatelessWidget {
         onTap: () => onTileTap(context, detection),
         child: Card(
           // Background color of the card
-          color: colorScheme.onPrimary,
+          color: ((jsonDecode(detection.boxes ?? "[]") as List).isNotEmpty) ? colorScheme.tertiary : colorScheme.onPrimary,
           child: Padding(
             padding: const EdgeInsets.all(11.0),
             child: Column(
@@ -152,7 +152,7 @@ class DetectionSmallListItem extends StatelessWidget {
       child: GestureDetector(
         onTap: () => onTileTap(context, detection),
         child: Card(
-          color: ((jsonDecode(detection.boxes ?? "[]") as List).isNotEmpty) ? mainColorScheme.tertiary :colorScheme.onPrimary,
+          color: ((jsonDecode(detection.boxes ?? "[]") as List).isNotEmpty) ? mainColorScheme.tertiary : colorScheme.onPrimary,
           child: ListTile(
             tileColor: ((jsonDecode(detection.boxes ?? "[]") as List).isNotEmpty) ? mainColorScheme.tertiary : null,
             leading: Container(
